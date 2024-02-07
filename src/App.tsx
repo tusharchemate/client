ed69e88e0fbca3616e5c707c79e16313e490bd82
Tushar <tusharchemate07@gmail.com>
2024-02-07 22:05:22 +0530

    added products endpoint in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://localhost:7157/api/Product')
+      .then((res) => res.json())
+      .then((res) =>
+        console.log('res', res)
+      )
+  }, []);
 
   return (
     <>
