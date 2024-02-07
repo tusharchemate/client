b94ce2bfb11839dccbd3e9cf2253fcc7517e631d
Tushar <tusharchemate07@gmail.com>
2024-02-08 01:25:33 +0530

    added styling in product card

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, List } from "antd";
+import { Avatar, Button, List } from "antd";
 import { Product } from "../../app/models/products";
 import './ProductCard.css';
 
@@ -10,8 +10,19 @@ const ProductCard = ({ product }: ProductCardProps) => {
     return (
         <List>
             <div className="productcardContainer">
-                <Avatar src={product.pictureURL}></Avatar>
-                <div>{product.name}</div>
+                <div className="image">
+                    <Avatar style={{ background: "none" }} src={product.pictureURL}></Avatar>
+                </div>
+                <div className="body">
+                    {product.name}
+                    <div>{product.discription}</div>
+                </div>
+
+                <div className="actions">
+                    <Button>Test</Button>
+                    <Button>Test2</Button>
+                </div>
+
             </div>
 
         </List>
